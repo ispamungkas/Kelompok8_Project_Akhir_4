6c8a1ae06f08591f3bce5218936421c71e81b35e
Lemenn <mrlmens01001@gmail.com>
2023-11-22 22:33:59 +0700

    Initial Commit

---
+++ app/src/main/java/com/uknown/kelompok8_project_akhir_4/SplachScreen.java
@@ -0,0 +1,16 @@
+package com.uknown.kelompok8_project_akhir_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uknown.kelompok8_project_akhir_4.R;
+
+public class SplachScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
---
+++ app/src/main/java/com/uknown/kelompok8_project_akhir_4/homepage/HomepageActivity.java
@@ -0,0 +1,16 @@
+package com.uknown.kelompok8_project_akhir_4.homepage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uknown.kelompok8_project_akhir_4.R;
+
+public class HomepageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
