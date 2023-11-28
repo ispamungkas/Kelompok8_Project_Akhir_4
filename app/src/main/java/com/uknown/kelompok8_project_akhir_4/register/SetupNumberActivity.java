64d066eb47dbe5cbbb30439c6c554c838ddd6ee2
imunFaisal <imunfaizal99@gmail.com>
2023-11-28 11:53:11 +0700

    register layout

---
+++ app/src/main/java/com/uknown/kelompok8_project_akhir_4/register/SetupNumberActivity.java
@@ -0,0 +1,16 @@
+package com.uknown.kelompok8_project_akhir_4.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.uknown.kelompok8_project_akhir_8.R;
+
+public class SetupNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setup_number);
+    }
+}
--- app/src/main/java/com/uknown/kelompok8_project_akhir_4/register/SplachScreen.java
+++ app/src/main/java/com/uknown/kelompok8_project_akhir_4/register/SplachScreen.java
@@ -4,13 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.uknown.kelompok8_project_akhir_4.R;
+import com.uknown.kelompok8_project_akhir_8.R;
+
 
 public class SplachScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+        setContentView(R.layout.splashscreen_layout);
     }
 }
